5b5bb3fa476d69843a412f3d321393d58d25d346
Lucian <lucian.costa.ext@synchro.com.br>
2021-07-28 22:04:45 -0300

    criando tela de login

--- ask-ufcg/src/App.js
+++ ask-ufcg/src/App.js
@@ -1,11 +1,15 @@
-import Login from './page/Login/login'
-
-function App() {
-  return (
-    <div>
-      <Login />
-    </div>
-  )
+import React from 'react';
+import Login from './page/Login/login';
+import { observer } from 'mobx-react';
+@observer
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Login />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- ask-ufcg/src/page/Login/login.js
+++ ask-ufcg/src/page/Login/login.js
@@ -1,39 +1,94 @@
-import './login.css'
-import React from 'react'
-import { Layout, Menu, Breadcrumb, Row, Input, Button } from 'antd'
-import Form from 'antd/lib/form/Form'
-const { Header, Content, Footer } = Layout
+import './login.css';
+import React from 'react';
+import { Layout, Row, Input, Button, Form, Col } from 'antd';
+const { Header, Content, Footer } = Layout;
+import { observer } from 'mobx-react';
 
+@observer
 class Login extends React.Component {
+  onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   render() {
     return (
       <>
-        <Layout className="layout">
-          <Header>
-            <div className="logo" />
-          </Header>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Header style={{ backgroundColor: '#A8DADC' }}></Header>
           <Content
             style={{
-              padding: '0 50px',
               textAlign: 'center',
+              padding: '0 50px',
             }}
           >
             <Row>
-              <h1 className="title-login">Sentimos Sua Falta</h1>
-            </Row>
-            <Row style={{ margin: '0px' }}>
-              <h3 className="subtitle-login">Vamos em busca do conhecimento coletivo</h3>
-            </Row>
-            <Row>
-              <Form></Form>
+              <Col xs={2} sm={4} md={6} lg={8} xl={9} />
+              <Col
+                xs={20}
+                sm={16}
+                md={12}
+                lg={12}
+                xl={6}
+                className='center-content'
+              >
+                <Row style={{ textAlign: 'left' }}>
+                  <Col span={24}>
+                    <h1 className='title-login'>Sentimos Sua Falta!</h1>
+                  </Col>
+                </Row>
+                <Row style={{ textAlign: 'left' }}>
+                  <Col span={24}>
+                    <h3 className='subtitle-login'>
+                      Vamos em busca do conhecimento coletivo
+                    </h3>
+                  </Col>
+                </Row>
+                <Form
+                  onFinish={this.onFinish}
+                  onFinishFailed={this.onFinishFailed}
+                >
+                  <Form.Item>
+                    <Row>
+                      <Col span={24}>
+                        <Input placeholder={'E-mail'} />
+                      </Col>
+                    </Row>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Row>
+                      <Col span={24}>
+                        <Input.Password placeholder={'Senha'} />
+                      </Col>
+                    </Row>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button
+                      type='primary'
+                      htmlType='submit'
+                      className='style-button'
+                    >
+                      Entrar
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Col>
+              <Col xs={2} sm={4} md={6} lg={8} xl={9} />
             </Row>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: 'center' }}>
+            Ask UFCG ©2021 Created by a Group of UFCG Students
+          </Footer>
         </Layout>
       </>
-    )
+    );
   }
 }
-Login.displayName = 'Login'
+Login.displayName = 'Login';
 
-export default Login
+export default Login;
