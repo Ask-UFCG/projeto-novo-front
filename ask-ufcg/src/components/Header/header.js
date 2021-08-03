33e8a7eeee37c0e6e5803b89d01e29f389538df3
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-08-03 13:22:42 -0300

    corrige estilo do header

--- ask-ufcg/src/components/Header/header.js
+++ ask-ufcg/src/components/Header/header.js
@@ -14,7 +14,7 @@ class Header extends React.Component {
   constructor(props) {
     super(props)
     this.store = new SessionStore()
-    this.title = useInfo()
+    //this.title = useInfo()
   }
 
   _renderButtonsLoginRegister = () => {
@@ -32,7 +32,7 @@ class Header extends React.Component {
         <Link to={NEW_ASK.route}>
           <Button
             type="primary"
-            style={{ width: '100%', backgroundColor: '#1D3557', borderColor: '#1D3557' }}
+            className="button-user-links"
           >
             {NEW_ASK.text}
           </Button>
@@ -49,33 +49,24 @@ class Header extends React.Component {
 
   render() {
     return (
-      <Layout.Header style={{ backgroundColor: '#A8DADC', width: '100%', display: 'flex' }}>
+      <div className="header">
         <div className="logo-header">
           <AskUFCGLogo className="logo-ask" />
           <p>
             Ask-<span className="logo-UFCG">UFCG</span>
           </p>
         </div>
-        <Row>
-          <Col span={8}>
-            <div className="title-header">{this.title}</div>
-          </Col>
-          <Col
-            span={8}
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              gap: '3rem',
-              alignItems: 'center',
-            }}
-          >
-            {this.store.authUser ? this._renderUserLinks() : this._renderButtonsLoginRegister()}
-          </Col>
-        </Row>
-      </Layout.Header>
+        <div className="title-header">
+          Title Page
+        </div>
+        <div className="user-links">
+          {this.store.authUser ? 
+              this._renderUserLinks() : this._renderButtonsLoginRegister()}
+        </div>
+      </div>
     )
   }
 }
+
 Header.displayName = 'Header'
 export default Header
