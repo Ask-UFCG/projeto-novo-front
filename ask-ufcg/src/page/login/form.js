b900440e0c2532706e1b0a026606d94c6c39b6cb
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-08-09 16:58:58 -0300

    normaliza a pagina de login

--- ask-ufcg/src/page/login/form.js
+++ ask-ufcg/src/page/login/form.js
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react';
 import './form.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Layout, Row, Input, Button, Form, Col, Divider } from 'antd';
 import User from '../../domain/user';
 import UserService from '../../services/user';
@@ -27,89 +28,55 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <>
-        <Content
-          style={{
-            textAlign: 'center',
-            padding: '0 50px',
-          }}
-        >
-          <Row>
-            <Col xs={2} sm={4} md={6} lg={8} xl={9} />
-            <Col
-              xs={20}
-              sm={16}
-              md={12}
-              lg={12}
-              xl={6}
-              className='center-content'
-            >
-              <Row style={{ textAlign: 'left' }}>
-                <Col span={24}>
-                  <h1 className='title-login'>Sentimos Sua Falta!</h1>
-                </Col>
-              </Row>
-              <Row style={{ textAlign: 'left' }}>
-                <Col span={24}>
-                  <h3 className='subtitle-login'>
-                    Vamos em busca do conhecimento coletivo
-                  </h3>
-                </Col>
-              </Row>
-              <Form
-                onFinish={this.onFinish}
-                onFinishFailed={this.onFinishFailed}
+      <div className='login-page'>
+        <div className='login-content'>
+          <h1 className='title-login'>Sentimos Sua Falta!</h1>
+          <h3 className='subtitle-login'>
+            Vamos em busca do conhecimento coletivo
+          </h3>
+          <Form
+            onFinish={this.onFinish}
+            onFinishFailed={this.onFinishFailed}
+          >
+            <Form.Item>
+              <Input
+                placeholder={'E-mail'}
+                onChange={(value) =>
+                  this.store.updateAttributeDecoratorKeyEventValue(
+                    'email',
+                    value
+                  )
+                }
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input.Password
+                placeholder={'Senha'}
+                onChange={(value) =>
+                  this.store.updateAttributeDecoratorKeyEventValue(
+                    'password',
+                    value
+                  )
+                }
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='style-button'
+                size={'large'}
               >
-                <Form.Item>
-                  <Row>
-                    <Col span={24}>
-                      <Input
-                        placeholder={'E-mail'}
-                        onChange={(value) =>
-                          this.store.updateAttributeDecoratorKeyEventValue(
-                            'email',
-                            value
-                          )
-                        }
-                      />
-                    </Col>
-                  </Row>
-                </Form.Item>
-                <Form.Item>
-                  <Row>
-                    <Col span={24}>
-                      <Input.Password
-                        placeholder={'Senha'}
-                        onChange={(value) =>
-                          this.store.updateAttributeDecoratorKeyEventValue(
-                            'password',
-                            value
-                          )
-                        }
-                      />
-                    </Col>
-                  </Row>
-                </Form.Item>
-                <Form.Item>
-                  <Button
-                    type='primary'
-                    htmlType='submit'
-                    className='style-button'
-                    size={'large'}
-                  >
-                    Entrar
-                  </Button>
-                </Form.Item>
-                <Divider />
-                <b>
-                  <a href='/register'>Não é registrado? Registre-se aqui!</a>
-                </b>
-              </Form>
-            </Col>
-            <Col xs={2} sm={4} md={6} lg={8} xl={9} />
-          </Row>
-        </Content>
-      </>
+                Entrar
+              </Button>
+            </Form.Item>
+            <Divider />
+          </Form>
+          <Link to='/register'>
+            Não é registrado? Registre-se aqui!
+          </Link>
+        </div>
+      </div>
     );
   }
 }
