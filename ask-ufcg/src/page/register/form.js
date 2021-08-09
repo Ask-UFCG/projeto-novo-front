39bc0f5abb3089a8a26e89923a55bd59cda96ec9
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-08-09 17:21:26 -0300

    normaliza a pagina de registro

--- ask-ufcg/src/page/register/form.js
+++ ask-ufcg/src/page/register/form.js
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react';
 import './form.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Layout, Row, Input, Button, Form, Col, Divider } from 'antd';
 import User from '../../domain/user';
 import UserService from '../../services/user';
@@ -30,126 +31,79 @@ class RegisterForm extends React.Component {
 
   render() {
     return (
-      <>
-        <Content
-          style={{
-            textAlign: 'center',
-            padding: '0 50px',
-          }}
-        >
-          <Row>
-            <Col xs={2} sm={4} md={6} lg={6} xl={6} />
-            <Col
-              xs={20}
-              sm={16}
-              md={12}
-              lg={12}
-              xl={12}
-              className='center-content'
-            >
-              <Row style={{ textAlign: 'left' }}>
-                <Col span={24}>
-                  <h1 className='title'>Entre na Comunidade Ask-UFCG</h1>
-                </Col>
-              </Row>
-              <Row style={{ textAlign: 'left' }}>
-                <Col span={24}>
-                  <h3 className='subtitle'>
-                    Obtenha mais recursos e privilegios
-                    <br />
-                    ingressando na comunidade mais útil
-                  </h3>
-                </Col>
-              </Row>
-              <Form
-                onFinish={this.onFinish}
-                onFinishFailed={this.onFinishFailed}
+      <div className='register-page'>
+        <div className='register-content'>
+          <h1 className='title'>Entre na Comunidade Ask-UFCG</h1>
+          <h3 className='subtitle'>
+            Obtenha mais recursos e privilegios
+            <br />
+            ingressando na comunidade mais útil
+          </h3>
+          <Form
+            onFinish={this.onFinish}
+            onFinishFailed={this.onFinishFailed}
+          >
+            <Form.Item>
+              <Input
+                placeholder={'Usuário'}
+                onChange={(value) =>
+                  this.store.updateAttributeDecoratorKeyEventValue(
+                    'usuario',
+                    value
+                  )
+                }
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                placeholder={'E-mail'}
+                onChange={(value) =>
+                  this.store.updateAttributeDecoratorKeyValue(
+                    'email',
+                    value
+                  )
+                }
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input.Password
+                placeholder={'Senha'}
+                onChange={(value) =>
+                  this.store.updateAttributeDecoratorKeyEventValue(
+                    'password',
+                    value
+                  )
+                }
+                maxLength={12}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input.Password
+                placeholder={'Repita a senha'}
+                disabled={!this.store.object.password}
+                onChange={(value) =>
+                  this.store.updateAttributeDecoratorKeyEventValue(
+                    'password',
+                    value
+                  )
+                }
+                maxLength={12}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='style-button'
               >
-                <Form.Item>
-                  <Row>
-                    <Col span={24}>
-                      <Input
-                        placeholder={'Usuário'}
-                        onChange={(value) =>
-                          this.store.updateAttributeDecoratorKeyEventValue(
-                            'usuario',
-                            value
-                          )
-                        }
-                      />
-                    </Col>
-                  </Row>
-                </Form.Item>
-                <Form.Item>
-                  <Row>
-                    <Col span={24}>
-                      <Input
-                        placeholder={'E-mail'}
-                        onChange={(value) =>
-                          this.store.updateAttributeDecoratorKeyValue(
-                            'email',
-                            value
-                          )
-                        }
-                      />
-                    </Col>
-                  </Row>
-                </Form.Item>
-
-                <Form.Item>
-                  <Row>
-                    <Col span={24}>
-                      <Input.Password
-                        placeholder={'Senha'}
-                        onChange={(value) =>
-                          this.store.updateAttributeDecoratorKeyEventValue(
-                            'password',
-                            value
-                          )
-                        }
-                        maxLength={12}
-                      />
-                    </Col>
-                  </Row>
-                </Form.Item>
-                <Form.Item>
-                  <Row>
-                    <Col span={24}>
-                      <Input.Password
-                        placeholder={'Repita a senha'}
-                        disabled={!this.store.object.password}
-                        onChange={(value) =>
-                          this.store.updateAttributeDecoratorKeyEventValue(
-                            'password',
-                            value
-                          )
-                        }
-                        maxLength={12}
-                      />
-                    </Col>
-                  </Row>
-                </Form.Item>
-
-                <Form.Item>
-                  <Button
-                    type='primary'
-                    htmlType='submit'
-                    className='style-button'
-                    size={'large'}
-                  >
-                    Entrar
-                  </Button>
-                </Form.Item>
-                <Divider />
-                <b>
-                  <a href='/login'>Já é registrado? Acesse aqui!</a>
-                </b>
-              </Form>
-            </Col>
-            <Col xs={2} sm={4} md={6} lg={6} xl={6} />
-          </Row>
-        </Content>
-      </>
+                Entrar
+              </Button>
+            </Form.Item>
+            <Divider />
+          </Form>
+          <Link to='/login'>Já é registrado? Acesse aqui!</Link>
+        </div>
+      </div>
     );
   }
 }
