f81cb375c34466cc4a048cbdce888d1500a685ea
Gabriel Souto <gabriel.souto.ext@synchro.com.br>
2021-08-12 14:00:50 -0300

    #10 - fix perfil

--- ask-ufcg/src/stores/profile/form.js
+++ ask-ufcg/src/stores/profile/form.js
@@ -1,59 +1,56 @@
-import { action, observable, runInAction, toJS } from 'mobx';
-import {
-  showErrorApiNotification,
-  showNotification,
-} from '../../utils/notification';
+import { action, observable, runInAction, toJS } from 'mobx'
+import { showErrorApiNotification, showNotification } from '../../utils/notification'
 
 class ProfileFormStore {
-  @observable object = {};
-  @observable loading = false;
+  @observable object = {}
+  @observable loading = false
 
   constructor(entity, service, entityName) {
-    this.entity = entity;
-    this.service = service;
-    this.entityName = entityName;
-    this.updateAttributeDecoratorKeyValue =
-      this.updateAttributeDecoratorKeyValue.bind(this);
+    this.entity = entity
+    this.service = service
+    this.entityName = entityName
+    this.updateAttributeDecoratorKeyValue = this.updateAttributeDecoratorKeyValue.bind(this)
 
     this.updateAttributeDecoratorKeyEventValue =
-      this.updateAttributeDecoratorKeyEventValue.bind(this);
+      this.updateAttributeDecoratorKeyEventValue.bind(this)
   }
 
   @action
   updateAttributeDecoratorKeyEventValue(key, event) {
-    this.object[key] = event.target.value;
+    this.object[key] = event.target.value
   }
 
   @action
   updateAttributeDecoratorKeyValue(key, value) {
-    this.object[key] = value;
+    this.object[key] = value
   }
 
   @action
   init(user) {
-    this.object = new this.entity(user);
+    this.object = new this.entity(user)
   }
 
   @action
   save(setUser, token) {
-    this.loading = true;
+    this.loading = true
     this.service
       .update(toJS(this.object), token)
       .then((response) => {
         runInAction(`Save User`, () => {
-          const content = response && response.data;
-          setUser({ user: new this.entity(content) });
-          showNotification('success', null, 'Usuário salvo com sucesso');
-          this.loading = false;
-        });
+          const content = response && response.data
+          debugger
+          setUser({ user: new this.entity(content) })
+          showNotification('success', null, 'Usuário salvo com sucesso')
+          this.loading = false
+        })
       })
       .catch((error) => {
         runInAction(`error on Save User`, () => {
-          this.loading = false;
-          showErrorApiNotification(error);
-        });
-      });
+          this.loading = false
+          showErrorApiNotification(error)
+        })
+      })
   }
 }
 
-export default ProfileFormStore;
+export default ProfileFormStore
