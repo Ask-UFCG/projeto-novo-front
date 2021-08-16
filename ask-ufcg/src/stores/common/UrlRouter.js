5b8efbdb751915c487389d6169b798a602dd841b
Gabriel Souto <gabriel.souto.ext@synchro.com.br>
2021-08-16 11:04:10 -0300

    #10 - add Url da pergunta

--- ask-ufcg/src/stores/common/UrlRouter.js
+++ ask-ufcg/src/stores/common/UrlRouter.js
@@ -4,3 +4,4 @@ export const SIGN_IN = { route: '/login', text: 'Entrar' }
 export const REGISTER = { route: '/register', text: 'Registrar' }
 export const NEW_ASK = { route: '/new-ask', text: 'Faça Uma Pergunta' }
 export const RULES = { route: '/rules', text: 'Regras Da Comunidade' }
+export const ASK = { route: '/ask', text: 'Pergunta' }
