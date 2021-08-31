f1372232d2c7d0a817cc65b93a44164fec376e61
gabrielmbs <gabriel.souto@ccc.ufcg.edu.br>
2021-08-31 14:07:22 -0300

    exibir

--- ask-ufcg/src/components/Answer/answer.js
+++ ask-ufcg/src/components/Answer/answer.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import { Button, Divider, Form, Input, Tooltip } from 'antd';
-import { observer } from 'mobx-react';
-import Answer from '../../domain/answer';
-import AnswerService from '../../services/answer';
-import { ReactComponent as MessageIcon } from '../../assets/message-square.svg';
-import Comment from '../Comment/index.js';
-import './answer.css';
-import AnswerFormStore from '../../stores/answer/form';
-import { userContext } from '../../userContext';
-import { getValueDateWithHours } from '../../utils/date';
-import imageNotFound from '../../assets/perfil_not_found.png';
-import { CheckCircleOutlined } from '@ant-design/icons';
+import React from 'react'
+import { Button, Divider, Form, Input, Tooltip } from 'antd'
+import { observer } from 'mobx-react'
+import Answer from '../../domain/answer'
+import AnswerService from '../../services/answer'
+import { ReactComponent as MessageIcon } from '../../assets/message-square.svg'
+import Comment from '../Comment/index.js'
+import './answer.css'
+import AnswerFormStore from '../../stores/answer/form'
+import { userContext } from '../../userContext'
+import { getValueDateWithHours } from '../../utils/date'
+import imageNotFound from '../../assets/perfil_not_found.png'
+import { CheckCircleOutlined } from '@ant-design/icons'
 
 @observer
 class AnswerComponent extends React.Component {
-  formRef = React.createRef();
+  formRef = React.createRef()
 
   constructor(props) {
-    super(props);
-    this.store = new AnswerFormStore(Answer, AnswerService, 'Resposta ');
-    this.idAuthorQuestion = this.props.idAuthorQuestion;
-    this.state = { showInputComment: false };
+    super(props)
+    this.store = new AnswerFormStore(Answer, AnswerService, 'Resposta ')
+    this.idAuthorQuestion = this.props.idAuthorQuestion
+    this.state = { showInputComment: false, showComments: false }
   }
 
   componentDidMount() {
-    const { content } = this.props;
-    this.store.init(content);
+    const { content } = this.props
+    this.store.init(content)
   }
 
-  _handleShowInputComment() {
+  _handleShowInputComment = () => {
     this.setState((oldState) => ({
-      showInputComment: !this.state.showInputComment,
-    }));
+      showInputComment: !oldState.showInputComment,
+    }))
+  }
+
+  _handleShowComments = () => {
+    this.setState((oldState) => ({
+      showComments: !oldState.showComments,
+    }))
   }
 
   render() {
@@ -40,24 +46,20 @@ class AnswerComponent extends React.Component {
         <userContext.Consumer>
           {({ user, token }) => {
             return (
-              <div className='answer-content'>
-                <div className='answer-card'>
-                  <div className='user-answer-card'>
+              <div className="answer-content">
+                <div className="answer-card">
+                  <div className="user-answer-card">
                     <img
                       src={this.store.object.author.linkAvatar ?? imageNotFound}
-                      alt='nome do usuario'
+                      alt="nome do usuario"
                     />
                     <p>
-                      {this.store.object.author.firstName +
-                        ' ' +
-                        this.store.object.author.lastName}
+                      {this.store.object.author.firstName + ' ' + this.store.object.author.lastName}
                     </p>
-                    <div className='user-answer-card-solved'>
+                    <div className="user-answer-card-solved">
                       {this.store.object.solution ? (
-                        <Tooltip title='Solução para pergunta'>
-                          <CheckCircleOutlined
-                            style={{ color: 'green', fontSize: '200%' }}
-                          />
+                        <Tooltip title="Solução para pergunta">
+                          <CheckCircleOutlined style={{ color: 'green', fontSize: '200%' }} />
                         </Tooltip>
                       ) : !user ? (
                         ''
@@ -65,11 +67,8 @@ class AnswerComponent extends React.Component {
                         <Button
                           style={{ backgroundColor: '#47ff85' }}
                           onClick={() => {
-                            this.store.updateAttributeDecoratorKeyValue(
-                              'solution',
-                              true
-                            );
-                            this.store.markAsSolution(token);
+                            this.store.updateAttributeDecoratorKeyValue('solution', true)
+                            this.store.markAsSolution(token)
                           }}
                         >
                           Marcar como solução
@@ -80,65 +79,60 @@ class AnswerComponent extends React.Component {
                     </div>
                   </div>
 
-                  <p className='answer-description'>
-                    {this.store.object.content}
-                  </p>
+                  <p className="answer-description">{this.store.object.content}</p>
                   <Divider />
 
-                  <div className='date-align'>
-                    {'Postada em: ' +
-                      getValueDateWithHours(this.store.object.createdAt)}
-                    <div className='main-answer-add-comment'>
+                  <div className="date-align">
+                    {'Postada em: ' + getValueDateWithHours(this.store.object.createdAt)}
+                    <div className="main-answer-add-comment">
                       {user ? (
                         <Button
-                          type='primary'
-                          htmlType='submit'
-                          className='style-button'
+                          type="primary"
+                          htmlType="submit"
+                          className="style-button"
                           onClick={() => this._handleShowInputComment()}
                         >
-                          <MessageIcon className='sent-icon-answer' />
-                          {this.state.showInputComment
-                            ? 'Cancelar'
-                            : 'Adicionar um Comentário'}
+                          <MessageIcon className="sent-icon-answer" />
+                          {this.state.showInputComment ? 'Cancelar' : 'Adicionar um Comentário'}
                         </Button>
                       ) : (
                         ''
                       )}
+                      <Button
+                        type="primary"
+                        className="style-button"
+                        onClick={this._handleShowComments}
+                      >
+                        {this.state.showComments ? 'Esconder Comentários' : 'Exibir Comentários'}
+                      </Button>
                     </div>
                   </div>
                 </div>
-                {this.store.object.comments
-                  ? this.store.object.comments.map((comment) => (
-                      <Comment content={comment} />
-                    ))
+                {this.store.object.comments && this.state.showComments
+                  ? this.store.object.comments.map((comment) => <Comment content={comment} />)
                   : ''}
                 <div
-                  className='input-comment-card'
-                  style={
-                    !this.state.showInputComment ? { display: 'none' } : {}
-                  }
+                  className="input-comment-card"
+                  style={!this.state.showInputComment ? { display: 'none' } : {}}
                 >
-                  <Form layout='vertical' ref={this.formRef}>
+                  <Form layout="vertical" ref={this.formRef}>
                     <Form.Item>
                       <Input
                         value={this.store.comment.content}
                         onChange={(value) => {
-                          this.store.updateAttributeDecoratorKeyCommentEventValue(
-                            'content',
-                            value
-                          );
+                          this.store.updateAttributeDecoratorKeyCommentEventValue('content', value)
                         }}
                       />
                     </Form.Item>
                     {user ? (
                       <Form.Item>
                         <Button
-                          type='primary'
-                          htmlType='submit'
-                          className='style-button'
+                          type="primary"
+                          htmlType="submit"
+                          className="style-button"
                           onClick={() => this.store.addComment(user, token)}
                         >
-                          <MessageIcon className='sent-icon-answered' />
+                          <MessageIcon className="sent-icon-answered" />
                           Comentar
                         </Button>
                       </Form.Item>
@@ -148,13 +142,13 @@ class AnswerComponent extends React.Component {
                   </Form>
                 </div>
               </div>
-            );
+            )
           }}
         </userContext.Consumer>
-      );
+      )
     }
-    return '';
+    return ''
   }
 }
 
-export default AnswerComponent;
+export default AnswerComponent
--- ask-ufcg/src/components/Comment/index.js
+++ ask-ufcg/src/components/Comment/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-import { Divider } from 'antd';
-import './index.css';
-import { observer } from 'mobx-react';
-import { getValueDateWithHours } from '../../utils/date';
+import { Divider } from 'antd'
+import './index.css'
+import { observer } from 'mobx-react'
+import { getValueDateWithHours } from '../../utils/date'
 
 @observer
 class Comment extends React.Component {
   constructor(props) {
-    super(props);
-    this.content = this.props.content;
+    super(props)
+    this.content = this.props.content
   }
 
   render() {
     if (this.content) {
       return (
-        <div className='answer-comment-card'>
-          <p className='comment-description'>{this.content.content}</p>
+        <div className="answer-comment-card">
+          <p className="comment-description">{this.content.content}</p>
           <Divider />
-          <p className='user-comment'>
+          <p className="user-comment">
             {'Comentado por ' +
               this.content.author.firstName +
               ' ' +
@@ -27,10 +27,10 @@ class Comment extends React.Component {
               getValueDateWithHours(this.content.createdAt)}
           </p>
         </div>
-      );
+      )
     }
-    return 'Sem comentários';
+    return 'Sem comentários'
   }
 }
 
-export default Comment;
+export default Comment
